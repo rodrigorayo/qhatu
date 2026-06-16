dccaf4a25a918abbe3da8076ac2e61a9052ea658
rodrigorayo <rodrigorayomartinez@gmail.com>
2026-06-15 20:48:08 -0400

    config: update api baseUrl to render public endpoint

--- qhatu_app/lib/config.dart
+++ qhatu_app/lib/config.dart
@@ -1,5 +1,4 @@
 class Config {
-  // Configurado automáticamente a tu IP de red local: http://192.168.0.10:3000
-  // para que tu celular físico se conecte al servidor de tu computadora en la misma red Wi-Fi.
-  static const String baseUrl = 'http://192.168.0.10:3000';
+  // Configurado automáticamente a la dirección de tu servidor en la nube en Render
+  static const String baseUrl = 'https://qhatu-api.onrender.com';
 }
